73b8ce038cce1be7a9aa62c13004168e638fe60f
Peter Horton <phorton@softstackfactory.org>
2018-09-21 13:13:17 +0000

    Sleep time

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -21,22 +21,55 @@ export class ChartsComponent  {
   pricesArr2;
   timeSeries;
   symbolClicked: boolean = false;
+  // intraday: string = 'function=TIME_SERIES_INTRADAY';
+  // daily: string = 'function=TIME_SERIES_DAILY';
+  // weekly: string = "function=TIME_SERIES_WEEKLY";
+  // monthly: string = "function=TIME_SERIES_MONTHLY_ADJUSTED";
+  
   
   constructor(private stk: StockService, public userService : UserService, private router: Router) {
     
   }
+  
+  public lineChartData:Array<any> = [];
+  public lineChartLabels:Array<any> = [];
+  
+  public lineChartType:string = 'line';
+  
+  public lineChartColors:Array<any> = [
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+  ];
+  
+  public lineChartOptions = {
+    legend: {
+      display: false,
+        labels: {
+          display: false
+        }
+    }
+  }  
   
   searchSymbol() {
     this.symbolClicked = true;
     console.log(this.symbol);
     this.stk.getData(this.symbol)
     .subscribe(res => {
-      let currentDay = moment().format("YYYY-MM-DD");
+      // let currentDay = moment().format("YYYY-MM-DD");
       let yesterday = moment().subtract(1, 'day').format("YYYY-MM-DD");
+      let dayBefore = moment().subtract(2, 'day').format("YYYY-MM-DD");
       // this.symbol = res["Meta Data"]["2. Symbol"];
-      this.price = Number(res["Time Series (Daily)"][currentDay]["4. close"]).toFixed(2);
-      this.percentChange = (((res["Time Series (Daily)"][currentDay]["4. close"]- res["Time Series (Daily)"][yesterday]["4. close"])/ res["Time Series (Daily)"][yesterday]["4. close"]) * 100).toFixed(2) + '%';
-      this.pricesArr = Object.keys(res["Time Series (Daily)"]).map(key => Number(res["Time Series (Daily)"][key]["4. close"]).toFixed(2)).reverse();
+      this.price = '$' + Number(res["Time Series (Daily)"][yesterday]["4. close"]).toFixed(2);
+      this.percentChange = (((res["Time Series (Daily)"][yesterday]["4. close"]- res["Time Series (Daily)"][dayBefore]["4. close"])/ res["Time Series (Daily)"][dayBefore]["4. close"]) * 100).toFixed(2) + '%';
+      this.lineChartData = Object.keys(res["Time Series (Daily)"]).map(key => Number(res["Time Series (Daily)"][key]["4. close"]).toFixed(2)).reverse();
+      console.log(Object.keys(res["Time Series (Daily)"]).reverse());
+      // this.lineChartLabels = Object.keys(res["Time Series (Daily)"]).reverse();
       console.log(res)
       }, err => {
         console.log(err)
@@ -55,24 +88,6 @@ export class ChartsComponent  {
   
   ngOnInit() {
   }
-  
-  public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'}
-  ];
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  
-  public lineChartType:string = 'line';
-  
-  public lineChartColors:Array<any> = [
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    },
-  ];
   
   // public chartHovered(e:any):void {
   //   console.log(e);
